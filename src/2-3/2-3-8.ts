5ea942d3cb774187e4ad2744b305c1ce394bc4eb
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-09-27 15:03:29 +0900

    2.3.8 nullとundefined

---
+++ src/2-3/2-3-8.ts
@@ -0,0 +1,11 @@
+export default {};
+const val1 = null;
+const val2 = undefined;
+
+console.log(val1, val2);
+
+// 型名もnullとundefined
+const n: null = null;
+const u: undefined = undefined;
+
+console.log(n, u);
