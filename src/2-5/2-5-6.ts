2ad21138563f7a2b55f48a9af9b02541059a9a44
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-09-30 23:42:47 +0900

    2.5.6 for文

---
+++ src/2-5/2-5-6.ts
@@ -0,0 +1,15 @@
+export default {};
+// for文を使って1から100まで出した和を表示するプログラム
+let sum = 0;
+for(let i: number = 1; i <= 100; i++){
+  sum += i;
+}
+console.log(sum); //5050
+
+// 1から100までの数字で偶数だけconsole.logに書き出すfor文
+// for(let i: number = 1; i <= 100; i++){
+//   if(i % 2 === 1){ // iが奇数の時true
+//     continue; // for文の先頭に戻る
+//   }
+//   console.log(i); // iが偶数の時実行される
+// }
