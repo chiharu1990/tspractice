b4d42822e16bad259d49758ab2b03515627160fb
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-05 20:09:09 +0900

    3.1.4 プロパティアクセス：値の取得と代入

---
+++ src/3-1/3-1-4.ts
@@ -0,0 +1,28 @@
+export default {};
+// プロパティに代入する
+const user = {
+  name: "uhyo",
+  age: 25,
+};
+
+user.age = 26;
+console.log(user.age); //26
+
+//プロパティ名を動的に決める
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const messeges = {
+  good: "0以上の値が入力されました",
+  bad: "負の数値を入力しないでください"
+}
+
+rl.question("数値を入力してください", (line) => {
+  const num = Number(line);
+  console.log(messeges[num >= 0 ? "good":"bad"]);
+  rl.close();
+});
