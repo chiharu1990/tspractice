fe6cd1bd22de2dd59565fed0e3ca701f587d606e
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-08 11:37:14 +0900

    3.4.1 型引数を持つ型を宣言する

---
+++ src/3-4/3-4-1.ts
@@ -0,0 +1,14 @@
+export default {};
+// 型引数Tを持つUser型を宣言している
+type User<T> = {
+  name: string;
+  child: T;
+}
+// 「User<T>型はnameプロパティがstring型であり、childプロパティはT型であるオブジェクトの型」ということになる
+
+//  型引が複数ある場合
+type Family<Parent,Child> = {
+  mother: Parent;
+  father: Parent;
+  child: Child;
+}
