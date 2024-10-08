9dcab5693c61b82ad8e7cf75beec9139c20f0c17
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-08 12:32:29 +0900

    3.4.2 型引数を持つ型を使用する

---
+++ src/3-4/3-4-2.ts
@@ -0,0 +1,21 @@
+export default {};
+type Family<Parent,Child> = {
+  mother: Parent;
+  father: Parent;
+  child: Child;
+}
+
+const obj:Family<number,string> = {
+  mother: 0,
+  father: 100,
+  child: "1000"
+};
+// Family<number,string>型は{mother: number; father: number; child: number;}型と同じ
+
+// 適切な数の型引数を指定せずに型を使用した場合はコンパイルエラーとなる
+// エラー：Generic type 'Family' requires 2 type argument(s).
+// const obj:Family = {
+//   mother: 0,
+//   father: 100,
+//   child: "1000"
+// };
