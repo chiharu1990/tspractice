9aa33cde535b85ec766a274aba3ec485b7660c0c
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-12 17:12:41 +0900

    3.6.2 オブジェクトの分割代入（2）ネストしたパターン

---
+++ src/3-6/3-6-2.ts
@@ -0,0 +1,17 @@
+export default {};
+// ネストしたオブジェクト
+const nested = {
+  num: 123,
+  obj: {
+    foo: "hello",
+    bar: "world",
+    baz:{
+      pee: 1
+    }
+  }
+}
+
+const {num, obj: {foo,baz:{pee}}} = nested;
+console.log(num); //123
+console.log(foo); //"hello"
+console.log(pee); //1
