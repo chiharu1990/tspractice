c4bfa89f8f38bd430d33167cafd0d8d246a58767
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-17 15:55:53 +0900

    4.1.1 関数宣言で関数を作る

---
+++ src/4-1/4-1-1.ts
@@ -0,0 +1,27 @@
+export default {};
+//　関数宣言の例
+// function range(min: number, max: number): number[] {
+//   const result = [];
+//   for(let i = min; i <= max; i++){
+//     result.push(i);
+//   }
+//   return result;
+// }
+
+// console.log(range(5,10)); //[ 5, 6, 7, 8, 9, 10 ]
+
+// 引数の数や、型を間違えるとコンパイルエラーとなる
+// console.log(range("5","10")); // エラー：Argument of type 'string' is not assignable to parameter of type 'number'.
+// console.log(range(5)); // エラー： Expected 2 arguments, but got 1.
+
+// 関数宣言の巻き上げ
+
+console.log(range(5,10)); //[ 5, 6, 7, 8, 9, 10 ]
+
+function range(min: number, max: number): number[] {
+  const result = [];
+  for(let i = min; i <= max; i++){
+    result.push(i);
+  }
+  return result;
+}
