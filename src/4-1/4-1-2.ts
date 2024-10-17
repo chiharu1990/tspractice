9a3cbf334f2c888668c53f8708ab417d8c69fb38
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-17 16:20:26 +0900

    4.1.2 返り値がない関数を作る

---
+++ src/4-1/4-1-2.ts
@@ -0,0 +1,22 @@
+export default {};
+// 返り値のない関数
+// function helloWorldNTimes(n: number): void {
+//   for(let i = 0; i < n; i++){
+//     console.log("Hello World!");
+//   }
+// }
+// helloWorldNTimes(3);
+
+// 早期リターン
+function helloWorldNTimes(n: number): void {
+  if(n >= 100){ // 100以上の数値が入力されたらループせずに別のメッセージを出す
+    console.log(`${n}回なんて無理です！`);
+    return;
+  }
+  for(let i = 0; i < n; i++){
+    console.log("Hello World!");
+  }
+}
+
+helloWorldNTimes(150);
+helloWorldNTimes(3);
