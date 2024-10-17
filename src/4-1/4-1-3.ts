a170994c87f31d842bcd8966043ae9d2c2050548
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-17 16:54:40 +0900

    4.1.3 関数式で関数を作る

---
+++ src/4-1/4-1-3.ts
@@ -0,0 +1,26 @@
+export default {};
+// 関数式で関数を作る
+type Human = {
+  height: number;
+  weight: number;
+};
+// const calcBMI = function(human: Human): number {
+//   return human.weight / human.height ** 2;
+// };
+// const uhyo: Human = {
+//   height:  1.84,
+//   weight: 72
+// }
+
+// console.log(calcBMI(uhyo));
+
+// 関数の引数に対して分割代入をする
+const calcBMI = function({height,weight}: Human): number {
+  return weight / height ** 2;
+};
+const uhyo: Human = {
+  height:  1.84,
+  weight: 72
+}
+
+console.log(calcBMI(uhyo));
