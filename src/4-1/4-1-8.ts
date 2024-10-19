3a7ee65600ec9439c3c0770aeab5d4f48ed1e67f
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-19 23:21:04 +0900

    4.1.8 関数呼び出しにおけるスプレッド構文

---
+++ src/4-1/4-1-8.ts
@@ -0,0 +1,23 @@
+export default {};
+const sum = (...args: number[]): number => {
+  let result = 0;
+  for(const num of args){
+    result += num;
+  }
+  return result;
+};
+
+// const nums = [1, 2, 3, 4, 5];
+// console.log(sum(...nums)); // 15 
+
+// 以下の関数sum3をスプレッド構文を使って呼び出すことはできない
+// const sum3 = (a: number, b: number, c: number) => a + b + c;
+// const nums = [1, 2, 3];
+// console.log(sum3(...nums)); // エラー：A spread argument must either have a tuple type or be passed to a rest parameter.
+// sum3は3つの引数を受け取るところ、number[]型であるnumsの要素は3つとは限らず、3つの引数を渡せないかもしれないから
+// sum3(...nums)の型チェックは要素数不明の何らかの配列という扱いになる
+// このエラーを回避するには、numsの要素数が3であるという情報を肩に残す必要がある。
+// タプル型を用いて回避する
+// const sum3 = (a: number, b: number, c: number) => a + b + c;
+// const nums:[number, number, number]  = [1, 2, 3];
+// console.log(sum3(...nums)); // 6
