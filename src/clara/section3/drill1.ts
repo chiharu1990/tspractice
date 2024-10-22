d96a540b07a1be6d9cfab8b87226973a1c48761c
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-22 12:59:56 +0900

    問題1の修正

--- src/clara/section3/drill1.ts
+++ src/clara/section3/drill1.ts
@@ -2,7 +2,7 @@ export default {};
 // result がarr1のコピーとなるように書いてください。
 const arr1 = ['one', 'two', 'three'];
 
-const result = arr1;
+const result = [...arr1];
 
 // 以下の行は変更しない
 console.log(result); // [ "one", "two", "three" ]
