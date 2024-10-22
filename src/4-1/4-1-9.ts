878796e4b5dbcd2bb2dbb9895dcda6280072b4fd
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-22 14:17:53 +0900

    4.1.9 オプショナル引数の宣言

---
+++ src/4-1/4-1-9.ts
@@ -0,0 +1,26 @@
+export default {};
+// オプショナルな引数の宣言（デフォルト値を指定しない場合）
+// const toLowerOrUpper = (str: string, upper?: boolean): string => {
+//   if(upper){
+//     return str.toUpperCase();
+//   } else {
+//     return str.toLowerCase();
+//   }
+// }
+
+// console.log(toLowerOrUpper("Hello")); // "hello"
+// console.log(toLowerOrUpper("Hello", false)); // "hello"
+// console.log(toLowerOrUpper("Hello",true)); // "HELLO"
+
+// オプショナルな引数の宣言（デフォルト値を指定する場合）
+const toLowerOrUpper = (str: string, upper: boolean = false): string => {
+  if(upper){
+    return str.toUpperCase();
+  } else {
+    return str.toLowerCase();
+  }
+}
+
+console.log(toLowerOrUpper("Hello")); // "hello"
+console.log(toLowerOrUpper("Hello", false)); // "hello"
+console.log(toLowerOrUpper("Hello",true)); // "HELLO"
