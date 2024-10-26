527a39003ff5f1e56cd3c9b51754fdc519ea3686
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-10-26 22:20:14 +0900

    コラム17 メソッド記法と部分型関係

---
+++ src/4-3/column17.ts
@@ -0,0 +1,19 @@
+export default {};
+type HasName = {name: string};
+type HasNameAndAge = {name: string; age: number};
+type Obj = {
+  func: (arg: HasName) => string;
+  method(arg: HasName): string;
+}
+
+const something: Obj = {
+  func: user => user.name,
+  method: user => user.name,
+}
+
+const getAge = (user: HasNameAndAge) => String(user.age);
+
+// エラー：Type '(user: HAsNameAndAge) => string' is not assignable to type '(arg: HasName) => string'.
+// something.func = getAge;
+// これはエラーが発生しない
+something.method = getAge;
