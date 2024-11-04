8fe784bec718fced43f30c86a012b0937146e5a3
chiharu1990 <chiharufukuyama.1990@gmail.com>
2024-11-04 20:46:13 +0900

    Promiseと例外

---
+++ src/async/section8.js
@@ -0,0 +1,12 @@
+export default {};
+function throwPromise() {
+  return new Promise((resolve, reject) => {
+    // Promiseコンストラクタの中で例外は自動的にキャッチされrejectを呼ぶ
+    throw new Error("例外が発生");
+    // 例外が発生すると、これ以降のコンストラクタの処理は実行されない
+  });
+}
+
+throwPromise().catch(error => {
+  console.log(error.message); // => "例外が発生"
+});
